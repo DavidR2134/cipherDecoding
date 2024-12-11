142b79570da4c1ec6be747f3554f035d64f13aa5
DavidR2134 <davidrogers2134@gmail.com>
2024-12-11 07:53:55 -0700

    add viginere decryption via filename

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import string
 
+
+def shift(letter):
+	alphabet = list(string.ascii_lowercase)
+	idx = alphabet.index(letter)
+
+	for i in range(idx):
+		tmp = alphabet[0]
+		alphabet.remove(tmp)
+		alphabet.append(tmp)
+
+	return "".join(alphabet)	
+
 def atbash(cipher):
 	alphabet = ["A","B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 	reverse_alphabet = ["A","B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -47,13 +59,41 @@ def a1z26(cipher):
 
 	return decoded
 
-def viginere(cipher, key):
-	pass
+def get_data(f):
+	d = []
+	with open(f, 'r') as text:
+		for line in text:
+			d.append(line.strip())	
+	return " ".join(d)
+
+def viginere(cipher_file):
+	text = get_data(cipher_file)
+	key = input("Please enter a key for the Viginere Cipher\n> ").lower()
+
+	key_alphabets = [list(shift(a)) for a in key]
+	decoded = ""
+	regular_old_alphabet = string.ascii_lowercase
+	key_idx = 0
+
+	for letter in text:
+		if letter == " ":
+			decoded += " "
+			continue
+
+		idx = key_alphabets[key_idx].index(letter.lower())
+		key_idx += 1
+
+		decoded += regular_old_alphabet[idx].upper()
+
+		if key_idx == len(key_alphabets):
+			key_idx = 0
+
+	print(decoded)
 
 def menu():
 	print("Welcome to the cipher decoder!")
 	print("=" * 50)
-	print("\n1) ATBASH Cipher\n2) Caesar Cipher\n3) A1Z26\n4) Quit")
+	print("\n1) ATBASH Cipher\n2) Caesar Cipher\n3) A1Z26\n4) Viginere\n5) Quit")
 
 def main():
 	#Season 1 Intro: VWDQ LV QRW ZKDW KH VHHPV - STAN IS NOT WHAT HE SEEMS
@@ -77,6 +117,9 @@ def main():
 		print(caesar(cipher))
 	elif choice == 3:
 		print(a1z26(cipher))
+	elif choice == 4:
+		print(viginere(cipher))
+
 
 if __name__ == "__main__":
 	main()
